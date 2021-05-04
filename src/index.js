53f9c0a1e001d926f71fbffafb581bce3eadadee
Cassandra <cparisi1290@gmail.com>
2021-05-04 11:37:14 -0500

    set up store and reducer in respected files

--- src/index.js
+++ src/index.js
@@ -3,19 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import store from './store'
 
-const users = () => []
-
-const reducer = combineReducers({
-  users
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE_ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render( 
   <Provider store={ store }>
