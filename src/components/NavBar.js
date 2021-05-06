cf83dbbb3d07d6d8308366724f7c987ded27126f
Cassandra <cparisi1290@gmail.com>
2021-05-06 17:31:09 -0500

    comment out login & logout imports since not in use yet

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Login from './Login'
-import Logout from './Logout';
+// import Login from './Login'
+// import Logout from './Logout';
 
 // functional component
 const NavBar = ({currentUser}) => {
