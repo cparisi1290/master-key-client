d79a14cadce4d794929300e314ec1105255370dd
Cassandra <cparisi1290@gmail.com>
2021-05-05 20:12:33 -0500

    move login/logout functionality to nav bar that gets current user info from store

---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Login from './Login'
+import Logout from './Logout';
+
+// functional component
+const NavBar = ({currentUser}) => {
+
+    return (
+        <div className="NavBar">
+            {currentUser ? <h2>Welcome Back, {currentUser.attributes.name}!</h2> : ""}
+            {currentUser ? <Logout/> : <Login/>}
+        </div>
+    )
+}
+
+// incoming arg is an obj, state, coming from redux,  which has a property called currentUser
+// state = { ...,
+//    currentUser: {
+//      name,
+//      value
+//  }
+// }
+const mapStateToProps = ({currentUser}) => {
+    return {
+      currentUser
+    }
+}
+
+export default connect(mapStateToProps)(NavBar) 
