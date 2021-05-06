daf27880f7ac86a0f287b1f2bba4f313d65a3cfb
Cassandra <cparisi1290@gmail.com>
2021-05-05 20:04:50 -0500

    create resetLoginForm to set login form back to initial state after being logged out

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -4,3 +4,9 @@ export const updateLoginForm = (formData) => {
         formData // payload
     }
 } 
+
+export const resetLoginForm = () => {
+    return {
+        type: "RESET_LOGIN_FORM"
+    }
+} 
