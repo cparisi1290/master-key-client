f5a83d6dddcc5836d82bc874a92a54f3ec1a5a94
Cassandra <cparisi1290@gmail.com>
2021-05-06 11:47:13 -0500

    fetch request to get property serialized data

---
+++ src/actions/myProperties.js
@@ -0,0 +1,30 @@
+// SYNC ACTION CREATORS 
+export const setMyProperties = (properties) => {
+    return {
+        type:"SET_MY_PROPERTIES",
+        properties // payload
+    }
+}
+
+// ASYNC ACTION CREATORS
+
+export const getMyProperties = () => {
+    return dispatch => {
+        return fetch("http://localhost:3001/api/v1/properties", {
+            credentials: "include",
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
+        .then(resp => resp.json())
+        .then(resp => {
+            if (resp.error) {
+                alert(resp.error)
+            } else {
+                dispatch(setMyProperties(resp.data))
+            }
+        })
+        .catch(console.log)
+    }
+}
