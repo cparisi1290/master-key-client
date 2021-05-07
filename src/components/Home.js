6a4ff7ed6326883fa9803df25e8040e5ca9e17ad
Cassandra <cparisi1290@gmail.com>
2021-05-07 13:06:54 -0500

    create landing page for user to login or sign up

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import Signup from './Signup'
+// import Login from './Login'
+// import Logout from './Logout'
+import { Link } from 'react-router-dom';
+
+
+// stateless component
+const Home = () => {
+    <div>
+        <h2><Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to acquire the Master Key!</h2>
+    </div>
+};
+
+export default Home;
