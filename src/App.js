083833715cec41ddc02b50f81bbf7c95251c36d8
Cassandra <cparisi1290@gmail.com>
2021-05-07 13:02:49 -0500

    remove navbar, logout component and main container component, add logged in conditional to include logout button if logged in, render propoerties if logged in, else show login page, pull out from store if user is logged in

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css';
 import React from 'react';
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser';
-import NavBar from './components/NavBar';
-import MainContainer from './components/MainContainer';
+// import NavBar from './components/NavBar';
+// import MainContainer from './components/MainContainer';
+import Home from './components/Home';
 import Login from './components/Login';
 import Logout from './components/Logout';
 import Signup from './components/Signup';
 import MyProperties from './components/MyProperties';
-import { Route } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 
 class App extends React.Component {
@@ -18,14 +19,17 @@ class App extends React.Component {
   }
   
   render() {
+    const { loggedIn } = this.props
     return (
       <div className="App">
-        <NavBar/>
-        <Logout/>
-        <MainContainer/>
-          <Route exact path='/signup' component={Signup}/>
+        { loggedIn ? <Logout /> : null}
+        <Switch>
+        <Route exact path='/signup' render={({history})=><Signup history={history}/>}/>
           <Route exact path='/login' component={Login}/>
+          <Route exact path='/' render={(props) => loggedIn ? <MyProperties {...props}/> : <Home {...props}/>}/>
           <Route exact path='/properties' component={MyProperties}/>
+          <Route exact path='/properties/new' component={NewProperty}/>
+        </Switch>
       </div>
     // <Footer/>
 
@@ -33,4 +37,10 @@ class App extends React.Component {
   }
 }
 
-export default connect(null, {getCurrentUser})(App);
+const mapStateToProps =state => {
+  return ({
+    loggedIn: !!state.currentUser
+  })
+}
+
+export default withRouter(connect(mapStateToProps, {getCurrentUser})(App));
