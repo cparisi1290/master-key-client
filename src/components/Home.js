3013cf2d18402d4a6f5a06a3920555ee8826cfdf
Cassandra <cparisi1290@gmail.com>
2021-05-07 13:35:07 -0500

    add return to function

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,10 +6,10 @@ import { Link } from 'react-router-dom';
 
 
 // stateless component
-const Home = () => {
-    <div>
-        <h2><Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to acquire the Master Key!</h2>
-    </div>
-};
+const Home = () => (
+        <div>
+            <h2><Link to="/login">Login</Link> or <Link to="/signup">Signup</Link> to acquire the Master Key!</h2>
+        </div>
+);
 
 export default Home;
