273942b6309f28f3c90852a9c2e0613412e26f85
Cassandra <cparisi1290@gmail.com>
2021-05-11 14:25:19 -0500

    add more attributes to property card

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 
-const PropertyCard = ({property}) => {
+const PropertyCard = ({ property }) => {
     return (
-        <p>{property.attributes.name}</p>
+        property ?
+            <div>
+                <p>{property.attributes.image}</p>
+                <h3>{property.attributes.name}</h3>
+                <p>{property.attributes.address}</p>
+                <p>{property.attributes.city}, {property.attributes.state}, {property.attributes.zip}</p>
+                <p>Bedrooms: {property.attributes.bedrooms}</p>
+                <p>Bathrooms: {property.attributes.bathrooms}</p>
+                <p>{property.attributes.occupied}</p>
+                <p>{property.attributes.pets_allowed}</p>
+                <p>Rent: ${property.attributes.rent_amount}</p>
+
+            </div> : 
+            <p>This is the property card.</p>
     )
 }
 
