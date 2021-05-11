5c794b90d98487f8245870874bb78e99d015ab55
Cassandra <cparisi1290@gmail.com>
2021-05-11 14:15:28 -0500

    create link on property index page to property show page

--- src/components/MyProperties.js
+++ src/components/MyProperties.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropertyCard from './PropertyCard'
+import { Link } from 'react-router-dom'
 
 const MyProperties = (props) => {
-    const propertyCards = props.myProperties.length >0 ? props.myProperties.map(p => <PropertyCard property={p} key={p.id}/>) : []
-    return (
-        propertyCards
-    )
+    const propertyCards = props.myProperties.length > 0 ? props.myProperties.map(p => (<p key={p.id}><Link to={`/properties/${p.id}`}>{p.attributes.name}</Link></p>)) : <p>This is propteries with an empty array of properties</p>
+    return propertyCards
 }
 
 // const mapStateToProps = state => {
