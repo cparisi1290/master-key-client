e74416318acd18231829897bac772f586f382c13
Cassandra <cparisi1290@gmail.com>
2021-05-11 14:32:46 -0500

    add new properties to reducer case statement, which returns current state with new property added onto it

--- src/reducers/myProperties.js
+++ src/reducers/myProperties.js
@@ -2,8 +2,10 @@ const property = (state = [], action) => {
     switch (action.type) {
         case "SET_MY_PROPERTIES":
             return action.properties
-        case "CLEAR_PROPERTIES" :
+        case "CLEAR_PROPERTIES":
             return []
+        case "ADD_PROPERTIES":
+            return state.concat(action.property)
         default: 
             return state
     }
