cc3d5ea81c71106ec340d2b372489d7b366a4656
Cassandra <cparisi1290@gmail.com>
2021-05-11 14:14:06 -0500

    create action to clear new property form after being submitted

--- src/actions/newPropertyForm.js
+++ src/actions/newPropertyForm.js
@@ -2,10 +2,15 @@
 
 export const updateNewPropertyForm = (name, value) => {
     const formData = {name, value}
-    console.log("formdata in acti on creaetor is", formData)
     return {
         type: "UPDATE_NEW_PROPERTY_FORM",
-        formData: {name, value}
+        formData
+    }
+}
+
+export const resetNewPropertyForm = () => {
+    return {
+        type: "RESET_NEW_PROPERTY_FORM"
     }
 }
 
