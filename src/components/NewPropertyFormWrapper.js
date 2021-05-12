98b7ac522e6016b6d3508d7b51438de86b2a143c
Cassandra <cparisi1290@gmail.com>
2021-05-12 13:39:49 -0500

    create file to handle new form

---
+++ src/components/NewPropertyFormWrapper.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createProperty } from '../actions/myProperties'
+import { connect } from 'react-redux'
+import PropertyForm from './PropertyForm';
+
+const NewPropertyFormWrapper = ({history, createProperty}) => {
+
+    const handleSubmit = (event, formData, userId, history) => {
+        createProperty({
+            ...formData,
+            userId,
+        }, history)            
+    }
+
+   return <PropertyForm history={history} handleSubmit={handleSubmit}/>
+}
+
+export default connect(null, {createProperty})(NewPropertyFormWrapper);
