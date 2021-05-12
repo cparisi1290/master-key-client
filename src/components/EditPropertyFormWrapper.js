6286b404eab76f760ba253e0b71ad187c6fd8463
Cassandra <cparisi1290@gmail.com>
2021-05-12 16:39:21 -0500

    add component did mount and unmount to load previous data if there was any to edit form and unmount it and clear form if going to new page

--- src/components/EditPropertyFormWrapper.js
+++ src/components/EditPropertyFormWrapper.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { setFormDataForEdit } from '../actions/propertyForm'
+import { setFormDataForEdit, resetPropertyForm } from '../actions/propertyForm'
 import { updateProperty } from '../actions/myProperties'
 import { connect } from 'react-redux'
 import PropertyForm from './PropertyForm';
@@ -7,15 +7,25 @@ import PropertyForm from './PropertyForm';
 class EditPropertyFormWrapper extends React.Component {
 
     componentDidMount() {
+        // where fetch request sare made
         this.props.property && this.props.setFormDataForEdit(this.props.property)
     }
-    
-    handleSubmit = (formData, userId) => {
+
+    componentDidUpdate(prevProps) {
+        // if property is loaded now and wasn't there before, then load previous data ex http://localhost:3000/properties/16/edit
+        this.props.property && !prevProps.property && this.props.setFormDataForEdit(this.props.property)
+    }
+
+    componentWillUnmount() {
+        // clear form if leaving edit property wrapper to new property form or anywhere else
+        this.props.resetPropertyForm()
+      }
+
+    handleSubmit = (formData) => {
         const {updateProperty, property, history} = this.props
         updateProperty({
             ...formData,
             propertyId: property.id,
-            userId,
         }, history)            
     }
 // wrapper has access to history
@@ -26,4 +36,4 @@ class EditPropertyFormWrapper extends React.Component {
    } 
 }
 
-export default connect(null, {updateProperty, setFormDataForEdit})(EditPropertyFormWrapper);
+export default connect(null, {updateProperty, setFormDataForEdit, resetPropertyForm})(EditPropertyFormWrapper);
