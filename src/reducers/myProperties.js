1ce2bede7ea38b68ba7471ca5f74dc74b56ab977
Cassandra <cparisi1290@gmail.com>
2021-05-13 01:44:49 -0500

    add delete reducer to find specific property and remove it from page

--- src/reducers/myProperties.js
+++ src/reducers/myProperties.js
@@ -6,12 +6,14 @@ const property = (state = [], action) => {
             return []
         case "ADD_PROPERTY":
             return state.concat(action.property)
-        case "UPDATE_PROPERTY":
-            // map over entire array in store, (map returns a copy, which is nondestructive, pure), property obj reps each member of prop array
-            // compare that id against incoming property id that's passed along as action.property
-            // if they match, update, if not, return current property
-            return state.map(property => property.id === action.property.id ? action.property : property)
-        default: 
+            case "UPDATE_PROPERTY":
+                // map over entire array in store, (map returns a copy, which is nondestructive, pure), property obj reps each member of prop array
+                // compare that id against incoming property id that's passed along as action.property
+                // if they match, update, if not, return current property
+                return state.map(property => property.id === action.property.id ? action.property : property)
+            case "DELETE_PROPERTY":
+                return state.filter(property => property.id === action.propertyId ? false : true)
+                default: 
             return state
     }
 }
