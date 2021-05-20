89107af2670faa81388358e741c1bf6942818047
Cassandra <cparisi1290@gmail.com>
2021-05-20 12:33:12 -0500

    delete commented out code

--- src/components/PropertyCard.js
+++ src/components/PropertyCard.js
@@ -40,17 +40,3 @@ const PropertyCard = ({ property }) => {
 }
 
 export default PropertyCard
-
-            // <div>
-            //     <p>{property.attributes.image}</p>
-            //     <h3>{property.attributes.name}</h3>
-            //     <p>{property.attributes.address}</p>
-            //     <p>{property.attributes.city}, {property.attributes.state}, {property.attributes.zip}</p>
-            //     <p>Bedrooms: {property.attributes.bedrooms}</p>
-            //     <p>Bathrooms: {property.attributes.bathrooms}</p>
-            //     <p>{property.attributes.occupied}</p>
-            //     <p>{property.attributes.pets_allowed}</p>
-            //     <p>Rent: ${property.attributes.rent_amount}</p>
-            //     <Link to={`/properties/${property.id}/edit`}>Edit Property</Link>
-            // </div> : 
-            // <p>This is the property card.</p>
